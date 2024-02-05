2831daa80379be8fa07359d2f93cb2c37eb0411a
maguitaria <t2glma00@students.oamk.fi>
2024-02-05 16:11:35 +0200

    MOdified colors and fonts for an app

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// day colors
+val card = Color(0xFF7D5260)
+val background = Color(0xFFF9F9F9)
+val primary = Color(0xFF006AF6)
+val text = Color(0xFF000000)
+
+// night colors
+val cardNight = Color(0xFF0A1A3A)
+val backgroundNight = Color(0xFF051025)
+val primaryNight = Color(0xFF509BFF)
+val textNight = Color(0xFF7D5260)
